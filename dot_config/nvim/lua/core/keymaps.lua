ec3bcff57e442c2ea26306bc2acca545c3b5a131
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-06-07 16:17:59 +0900

    add keymaps

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -5,3 +5,19 @@ key("", "<Space>", "<Nop>", opts)
 
 key("i", "jk", "<Esc>", opts)
 key("i", "jj", "<Esc>", opts)
+
+key("n", "L", "<Cmd>bnext<CR>", opts)
+key("n", "H", "<Cmd>bprev<CR>", opts)
+key("n", "<Leader>e", "<Cmd>Neotree<CR>", opts)
+
+--Telescope
+key("n", ";f", "<Cmd>Telescope find_files<CR>", opts)
+key("n", ";g", "<Cmd>Telescope kensaku<CR>", opts)
+key("n", ";lg", "<Cmd>Telescope live_grep<CR>", opts)
+key("n", ";b", "<Cmd>Telescope buffers<CR>", opts)
+
+--Toggleterm
+key("n", "<Leader>t", "<Cmd>ToggleTerm<CR>", opts)
+
+key("v", "<", "<gv", opts)
+key("v", ">", ">gv", opts)

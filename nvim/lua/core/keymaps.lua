48212d51c47ded70ba8c3a00a6d19880a47486f4
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-12-06 13:32:49 +0900

    update

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -25,3 +25,40 @@ key("n", "<C-Up>", ":resize -2<CR>", opts)
 key("n", "<C-Down>", ":resize +2<CR>", opts)
 key("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 key("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+vim.cmd [[
+" カーソルがインデント内部ならtrue
+function! s:in_indent() abort
+  return col('.') <= indent('.')
+endfunction
+
+" カーソルがインデントとずれた位置ならtrue
+function! s:not_fit_indent() abort
+  return !!((col('.') - 1) % shiftwidth())
+endfunction
+
+function! s:quantized_h(cnt = 1) abort
+  if a:cnt > 1 || !&expandtab
+    execute printf('normal! %sh', a:cnt)
+    return
+  endif
+  normal! h
+  while s:in_indent() && s:not_fit_indent()
+    normal! h
+  endwhile
+endfunction
+
+function! s:quantized_l(cnt = 1) abort
+  if a:cnt > 1 || !&expandtab
+    execute printf('normal! %sl', a:cnt)
+    return
+  endif
+  normal! l
+  while s:in_indent() && s:not_fit_indent()
+    normal! l
+  endwhile
+endfunction
+
+noremap h <cmd>call <sid>quantized_h(v:count1)<cr>
+noremap l <cmd>call <sid>quantized_l(v:count1)<cr>
+]]
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,9 +1,16 @@
 return {
-	{ "vim-denops/denops.vim", lazy = false },
-	{
-		"yuki-yano/denops-lazy.nvim",
-	},
-	{ "tani/vim-artemis" },
-	{ "nvim-lua/plenary.nvim", lazy = false },
-	{},
+  { "vim-denops/denops.vim", lazy = false },
+  {
+    "yuki-yano/denops-lazy.nvim",
+  },
+  { "tani/vim-artemis" },
+  { "nvim-lua/plenary.nvim", lazy = false },
+  {
+    "oflisback/obsidian-bridge.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    event = { "BufReadPre *.md", "BufNewFile *.md" },
+    opts = {
+      obsidian_server_address = "http://172.25.192.1:27123"
+    }
+  }
 }

eaa10f4b203565a3fd1a28d281abe6e81f4fdaea
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-09-21 20:45:33 +0900

    Update .config/nvim/lua/plugins/ui/colorscheme.lua

--- dot_config/nvim/lua/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/plugins/ui/colorscheme.lua
@@ -14,38 +14,38 @@ return {
           hl.CmpItemAbbrMatchFuzzy = { fg = c.blue, bg = "NONE", bold = true }
           hl.CmpItemMenu = { fg = c.bright_magenta, bg = "NONE", italic = true }
 
-          hl.CmpItemKindField = { fg = c.bg, bg = colors.red }
-          hl.CmpItemKindProperty = { fg = c.bg, bg = colors.red }
-          hl.CmpItemKindEvent = { fg = c.bg, bg = colors.red }
+          hl.CmpItemKindField = { fg = c.bg, bg = c.red }
+          hl.CmpItemKindProperty = { fg = c.bg, bg = c.red }
+          hl.CmpItemKindEvent = { fg = c.bg, bg = c.red }
 
-          hl.CmpItemKindText = { fg = c.bg, bg = colors.green }
-          hl.CmpItemKindEnum = { fg = c.bg, bg = colors.green }
-          hl.CmpItemKindKeyword = { fg = c.bg, bg = colors.green }
+          hl.CmpItemKindText = { fg = c.bg, bg = c.green }
+          hl.CmpItemKindEnum = { fg = c.bg, bg = c.green }
+          hl.CmpItemKindKeyword = { fg = c.bg, bg = c.green }
 
-          hl.CmpItemKindConstant = { fg = c.bg, bg = colors.yellow }
-          hl.CmpItemKindConstructor = { fg = c.bg, bg = colors.yellow }
-          hl.CmpItemKindReference = { fg = c.bg, bg = colors.yellow }
+          hl.CmpItemKindConstant = { fg = c.bg, bg = c.yellow }
+          hl.CmpItemKindConstructor = { fg = c.bg, bg = c.yellow }
+          hl.CmpItemKindReference = { fg = c.bg, bg = c.yellow }
 
-          hl.CmpItemKindFunction = { fg = c.bg, bg = colors.magenta }
-          hl.CmpItemKindStruct = { fg = c.bg, bg = colors.magenta }
-          hl.CmpItemKindClass = { fg = c.bg, bg = colors.magenta }
-          hl.CmpItemKindModule = { fg = c.bg, bg = colors.magenta }
-          hl.CmpItemKindOperator = { fg = c.bg, bg = colors.magenta }
+          hl.CmpItemKindFunction = { fg = c.bg, bg = c.magenta }
+          hl.CmpItemKindStruct = { fg = c.bg, bg = c.magenta }
+          hl.CmpItemKindClass = { fg = c.bg, bg = c.magenta }
+          hl.CmpItemKindModule = { fg = c.bg, bg = c.magenta }
+          hl.CmpItemKindOperator = { fg = c.bg, bg = c.magenta }
 
-          hl.CmpItemKindVariable = { fg = c.bg, bg = colors.cyan }
-          hl.CmpItemKindFile = { fg = c.bg, bg = colors.cyan }
+          hl.CmpItemKindVariable = { fg = c.bg, bg = c.cyan }
+          hl.CmpItemKindFile = { fg = c.bg, bg = c.cyan }
 
-          hl.CmpItemKindUnit = { fg = c.bg, bg = colors.orange }
-          hl.CmpItemKindSnippet = { fg = c.bg, bg = colors.orange }
-          hl.CmpItemKindFolder = { fg = c.bg, bg = colors.orange }
+          hl.CmpItemKindUnit = { fg = c.bg, bg = c.orange }
+          hl.CmpItemKindSnippet = { fg = c.bg, bg = c.orange }
+          hl.CmpItemKindFolder = { fg = c.bg, bg = c.orange }
 
-          hl.CmpItemKindMethod = { fg = c.bg, bg = colors.blue }
-          hl.CmpItemKindValue = { fg = c.bg, bg = colors.blue }
-          hl.CmpItemKindEnumMember = { fg = c.bg, bg = colors.blue }
+          hl.CmpItemKindMethod = { fg = c.bg, bg = c.blue }
+          hl.CmpItemKindValue = { fg = c.bg, bg = c.blue }
+          hl.CmpItemKindEnumMember = { fg = c.bg, bg = c.blue }
 
-          hl.CmpItemKindInterface = { fg = c.bg, bg = colors.purple }
-          hl.CmpItemKindColor = { fg = c.bg, bg = colors.purple }
-          hl.CmpItemKindTypeParameter = { fg = c.bg, bg = colors.purple }
+          hl.CmpItemKindInterface = { fg = c.bg, bg = c.purple }
+          hl.CmpItemKindColor = { fg = c.bg, bg = c.purple }
+          hl.CmpItemKindTypeParameter = { fg = c.bg, bg = c.purple }
         end,
         highlight_overrides = {
         },

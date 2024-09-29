37853f69e356ef538b5601cbe8ca156803c170fb
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-09-29 15:25:16 +0900

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -54,24 +54,16 @@ opt.smoothscroll = true
 opt.pumheight = 10
 
 if vim.fn.has("wsl") == 1 then
-  if vim.fn.executable("wl-copy") == 0 then
-    print("wl-clipboard not found, clipboard integration won't work")
-  else
-    vim.g.clipboard = {
-      name = "wl-clipboard (wsl)",
-      copy = {
-        ["+"] = 'wl-copy --foreground --type text/plain',
-        ["*"] = 'wl-copy --foreground --primary --type text/plain',
-      },
-      paste = {
-        ["+"] = (function()
-          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { '' }, 1)         -- '1' keeps empty lines
-        end),
-        ["*"] = (function()
-          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { '' }, 1)
-        end),
-      },
-      cache_enabled = true
-    }
-  end
+  vim.g.clipboard = {
+    name = "yank (wsl)",
+    copy = {
+      ["+"] = 'win32yank.exe -i',
+      ["*"] = 'win32yank.exe -i',
+    },
+    paste = {
+      ["+"] = 'win32yank.exe -o',
+      ["*"] = 'win32yank.exe -o',
+    },
+    cache_enabled = true
+  }
 end

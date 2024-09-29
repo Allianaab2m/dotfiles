b27f30b2e2ec596b3bc91d1a929f32a2ab30a36a
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-09-29 15:17:16 +0900

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,12 +6,12 @@ opt.conceallevel = 2
 opt.confirm = true
 opt.cursorline = true
 opt.fillchars = {
-	foldopen = "",
-	foldclose = "",
-	fold = " ",
-	foldsep = " ",
-	diff = "╱",
-	eob = " ",
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
 }
 opt.foldlevel = 99
 opt.formatoptions = "jcroqlnt"
@@ -52,3 +52,26 @@ opt.winminwidth = 5
 opt.wrap = false
 opt.smoothscroll = true
 opt.pumheight = 10
+
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-clipboard not found, clipboard integration won't work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = 'wl-copy --foreground --type text/plain',
+        ["*"] = 'wl-copy --foreground --primary --type text/plain',
+      },
+      paste = {
+        ["+"] = (function()
+          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { '' }, 1)         -- '1' keeps empty lines
+        end),
+        ["*"] = (function()
+          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { '' }, 1)
+        end),
+      },
+      cache_enabled = true
+    }
+  end
+end

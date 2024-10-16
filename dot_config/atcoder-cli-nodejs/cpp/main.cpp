f4b873f24d673d15cf0e076312c5951fe3fd90c7
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-10-16 13:03:17 +0900

    Update .config/atcoder-cli-nodejs/cpp/main.cpp

--- dot_config/atcoder-cli-nodejs/cpp/main.cpp
+++ dot_config/atcoder-cli-nodejs/cpp/main.cpp
@@ -8,8 +8,16 @@ using namespace std;
 
 #define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
 #define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define Yes cout << "Yes" << '\n'
+#define No cout << "No" << '\n'
+
+#define debug(x) cerr << #x << " = " << x << '\n'
 // }}}
 
-int main() {}
+int main() {
+  // Code here:
+  return 0;
+}
 
 // vim: foldmethod=marker foldlevel=0

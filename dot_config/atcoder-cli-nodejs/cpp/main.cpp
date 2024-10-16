9881034b049911a032c071d96a644f78756d9be1
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-10-16 11:40:20 +0900

    change cpp template

--- dot_config/atcoder-cli-nodejs/cpp/main.cpp
+++ dot_config/atcoder-cli-nodejs/cpp/main.cpp
@@ -1,4 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+//{{{
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+// }}}
 
 int main() {}
+
+// vim: foldmethod=marker foldlevel=0

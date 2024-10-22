d87dc93f60c47c0c4a50c1f608a473705350ec68
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-10-22 16:53:56 +0900

    Update .config/atcoder-cli-nodejs/cpp/main.cpp

--- dot_config/atcoder-cli-nodejs/cpp/main.cpp
+++ dot_config/atcoder-cli-nodejs/cpp/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 using ll = long long;
 ll INF = 2e18;
 
+// Vector
 template <typename T> using vc = vector<T>;
 template <typename T> using vv = vc<vc<T>>;
 using vl = vc<ll>;      // vector<ll>
@@ -13,8 +14,11 @@ using vvvvl = vv<vvl>;  // vector<vector<vector<vector<ll>>>>
 using vs = vc<string>;  // vector<string>
 using vvs = vv<string>; // vector<vector<string>>
 
+// CE when using brace([]) access in vector
+#ifdef ONLINE_JUDGE
 #define _GLIBCXX_DEBUG
 #define _LIBCPP_DEBUG 0
+#endif
 
 #define OVERLOAD_REP(_1, _2, _3, name, ...) name
 #define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
@@ -28,12 +32,16 @@ using vvs = vv<string>; // vector<vector<string>>
 #define No cout << "No" << '\n'
 
 #define debug(x) cerr << #x << " = " << x << '\n'
-                        //  }}}
+
+// cin >> a; for vector!
+template <class T> istream &operator>>(istream &i, vc<T> &v) {
+  rep(j, size(v)) i >> v[j];
+  return i;
+}
+//  }}}
 
 int main() {
   // Code here:
-  vl a(3);
-  cout << a[100] << endl;
   return 0;
 }
 

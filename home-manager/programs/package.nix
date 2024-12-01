25a323dc0f4d2345ce650d93f39bb3dae5560545
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-12-01 10:14:21 +0900

    chore(deps): add zellij

--- home-manager/programs/package.nix
+++ home-manager/programs/package.nix
@@ -2,7 +2,7 @@
 let
   # CLI Tools
   misc = with pkgs; [
-    bat delta fd ripgrep fzf lazygit tree eza which ghq fastfetch go-task jq _1password-cli curl unzip xdg-utils wget
+    bat delta fd ripgrep fzf lazygit tree eza which ghq fastfetch go-task jq _1password-cli curl unzip xdg-utils wget zellij
   ];
   # Language runtimes
   dev = with pkgs; [

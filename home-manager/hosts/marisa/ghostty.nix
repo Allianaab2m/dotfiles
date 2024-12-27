8b08e1f0608cb78a11c622d68935118a6e4bd0a2
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-12-28 02:55:03 +0900

    fix(ghostty): add PATH for using cli

--- home-manager/hosts/marisa/ghostty.nix
+++ home-manager/hosts/marisa/ghostty.nix
@@ -3,5 +3,15 @@
   home.file.".config/ghostty/config".text = ''
     font-family = "PlemolJP35 Console NF"
     theme = "catppuccin-mocha"
+    window-theme = "ghostty"
+    background-opacity = 0.85
+    background-blur-radius = 20
+    macos-titlebar-style = transparent
   '';
+  # for CLI
+  programs.zsh = {
+    envExtra = ''
+      export PATH=$PATH:/Applications/Ghostty.app/Contents/MacOS/
+    '';
+  };
 }

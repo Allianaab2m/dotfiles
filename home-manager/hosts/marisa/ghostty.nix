b1235ef8510beb9a0b1d21f617e7aaa9cb2b9c4e
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-12-31 01:09:22 +0900

    chore(ghostty): add some settings

--- home-manager/hosts/marisa/ghostty.nix
+++ home-manager/hosts/marisa/ghostty.nix
@@ -1,12 +1,25 @@
 { ... }:
 {
   home.file.".config/ghostty/config".text = ''
+    shell-integration = zsh
+
     font-family = "PlemolJP35 Console NF"
+
     theme = "catppuccin-mocha"
     window-theme = "ghostty"
+
     background-opacity = 0.85
     background-blur-radius = 20
+
     macos-titlebar-style = transparent
+
+    mouse-hide-while-typing = true
+
+    cursor-opacity = 0.7
+    cursor-style = block
+    cursor-style-blink = false
+    shell-integration-features = no-cursor
+    resize-overlay = never
   '';
   # for CLI
   programs.zsh = {

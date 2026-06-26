32b8ec2a32b05a87ed24f64e832686c95b876d19
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2026-06-26 16:57:36 +0900

    chore: add nix.settings

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -12,15 +12,20 @@
       experimental-features = "nix-command flakes";
       max-jobs = 8;
     };
-    extra-substituters = [
-      "https://cache.numtide.com"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    trusted-users = [ "root" "@wheel" ] ++ lib.optional pkgs.stdenv.isDarwin "@admin";
+
+    settings = {
+      extra-substituters = [
+        "https://cache.numtide.com"
+        "https://nix-community.cachix.org"
+        "https://ab2m.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ab2m.cachix.org-1:gQOvR3a+Ttk401dA5hf/i8LvwanRFrtt7TUc7u2/lto="
+      ];
+      trusted-users = [ "root" "@wheel" ] ++ lib.optional pkgs.stdenv.isDarwin "@admin";
+    };
   };
 
   security = {
